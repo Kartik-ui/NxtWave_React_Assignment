f6fa5330d0f5ebd79828ddf663e2f56fbab6f98f
Kartik <imkartik1311@gmail.com>
2025-02-22 19:03:31 +0530

    add updated deploy code

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/NxtWave_React_Assignment">
       <App />
     </BrowserRouter>
   </StrictMode>
