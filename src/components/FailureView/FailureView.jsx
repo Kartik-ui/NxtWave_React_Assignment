8fb9865b0f65de96677e4d985e59ade580f38012
Kartik <imkartik1311@gmail.com>
2025-02-22 20:28:49 +0530

    add updated failview

--- src/components/FailureView/FailureView.jsx
+++ src/components/FailureView/FailureView.jsx
@@ -1,11 +1,14 @@
+import failImage from "../../assets/list-creation-failure-lg-output.png";
 import "./FailView.css";
 
 const FailureView = ({ message, buttonText, handleClick }) => {
   return (
     <section className="fail_container">
-      <img src="/public/list-creation-failure-lg-output.png" alt="failView" />
+      <img src={failImage} alt="failView" />
       <h1>{message}</h1>
-      <button className="fail_button" onClick={handleClick}>{buttonText}</button>
+      <button className="fail_button" onClick={handleClick}>
+        {buttonText}
+      </button>
     </section>
   );
 };
